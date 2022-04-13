0166e5b76fa16b39106e94cbbad21ef501466669
Khorp <71438210+Khorp@users.noreply.github.com>
2022-04-13 11:03:44 +0200

    Gun feature
    you now have a gun and you can shoot it

---
+++ 2DPlatformerOnline/Assets/Scripts/ShootScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+
+    public Transform Gun;
+    public Animator gunAnimator;
+
+    Vector2 direction;
+
+    public GameObject Bullet;
+
+    public float BulletSpeed;
+    public Transform ShootPoint;
+    public float fireRate;
+    float ReadyForNextShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePos - (Vector2)Gun.position;
+        FaceMouse();
+
+        if (Input.GetMouseButton(0)) 
+        {
+            if (Time.time > ReadyForNextShot) 
+            {
+                ReadyForNextShot = Time.time + 1 / fireRate;
+                shoot();
+            }
+        }
+    }
+
+    void FaceMouse() 
+    {
+        Gun.transform.right = direction;
+    }
+    void shoot()
+    {
+        GameObject BulletIns = Instantiate(Bullet,ShootPoint.position,ShootPoint.rotation);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(BulletIns.transform.right * BulletSpeed);
+        gunAnimator.SetTrigger("Shoot");
+        Destroy(BulletIns,3);
+    }
+}
